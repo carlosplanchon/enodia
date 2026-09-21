54dfc7b77e6a6a021ecddde5e1bfe86178708e54
Carlos Andrés Planchón Prestes <carlosandresplanchonprestes@gmail.com>
2026-09-21 18:57:47 -0300

    pyproject.toml is now the only source of truth for package version.
    Signed-off-by: Carlos Andrés Planchón Prestes <carlosandresplanchonprestes@gmail.com>

--- enodia/__init__.py
+++ enodia/__init__.py
@@ -4,7 +4,9 @@ A talking Wi-Fi scanner in a backpack, a paper notebook of street crossings, and
 reconciliation that places every access point along the walk.
 """
 
-__version__ = "0.1.5"
+from importlib.metadata import version
+
+__version__ = version(__package__)
 
 from enodia.button import ButtonMarker, InputDevice, find_button_devices, list_input_devices
 from enodia.draw import Frame, svg_map
